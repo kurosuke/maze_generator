d6d3b0eb752f334f18969536ab087efa3aa59d2a
kuro <hato-github@yuzu.gr.jp>
2012-04-01 20:11:20 +0900

    make slove demo by web.

--- make_js.sh
+++ make_js.sh
@@ -8,6 +8,6 @@ do
 	echo "var matrix = [];" > maze_${NO}.js
 	echo "var n = 0;" >> maze_${NO}.js
 
-	python maze_generate.py 80 | grep -v '^$' | sed -e 's/^/matrix[n++] = "/' -e 's/$/";/' >> maze_${NO}.js
+	python maze_generate.py 80 | grep -v '^$' | sed -e 's/^/matrix[n++] = "/' -e 's/$/";/' >> htdocs/js/maze_${NO}.js
 	NO=`expr $NO + 1`
 done
